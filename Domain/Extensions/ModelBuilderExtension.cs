f1277e0bc2bc5b125e06cf8e4a5b1ad0eb81cea8
Amir Shayan <wahtamir12@gmail.com>
2024-01-01 11:08:19 +0330

    Added extension for DbContext for adding entities

---
+++ Domain/Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Extensions;
+
+public static class ModelBuilderExtension
+{
+    public static void AddAllEntities<TBaseType>(this ModelBuilder modelBuilder, params Assembly[] assemblies)
+    {
+        var entityTypes = assemblies
+            .SelectMany(x => x.GetExportedTypes())
+            .Where(p => p is { IsPublic: true, IsAbstract: false } && typeof(TBaseType)
+                            .IsAssignableFrom(p) &&
+                        p.IsSubclassOf(typeof(TBaseType)));
+        foreach (var type in entityTypes)
+        {
+            modelBuilder.Entity(type);
+        }
+    }
+}
--- Domain/EzCommerceContext.cs
+++ Domain/EzCommerceContext.cs
@@ -1,4 +1,6 @@
+using Domain.Entities.BaseEntity;
 using Domain.Entities.Identities;
+using Domain.Extensions;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,5 +11,13 @@ public class EzCommerceContext : IdentityDbContext<User,Role,long>
     public EzCommerceContext(DbContextOptions<EzCommerceContext> options): base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelbuilder)
+    {
+        base.OnModelCreating(modelbuilder);
+        var assemblies = typeof(IBaseEntity).Assembly;
+        modelbuilder.AddAllEntities<IBaseEntity>(assemblies);
+        modelbuilder.ApplyConfigurationsFromAssembly(typeof(IBaseEntity).Assembly);
     }
 }
